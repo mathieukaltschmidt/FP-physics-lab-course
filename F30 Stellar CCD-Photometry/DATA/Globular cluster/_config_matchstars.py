894280059a322e47c49be83e2217a8696ed03043
Mathieu Kaltschmidt <m.kaltschmidt97@gmail.com>
2018-09-03 10:38:05 +0200

    try

--- F30 Stellar CCD-Photometry/DATA/Globular cluster/_config_matchstars.py
+++
@@ -1,7 +0,0 @@
-# the lists created by STARFINDER (section 6.3)
-lst_psffit_V = "/home/fp30/praktikum/Asmita/Mathieu_Quirinus/stars_V.txt"  # define path to PSF fitting list for V filter
-lst_psffit_I = "/home/fp30/praktikum/Asmita/Mathieu_Quirinus/stars_I.txt"  # define path to PSF fitting list for I filter
-
-# the values of the zeropoints calculated in section 6.4
-zero_V = 25.334
-zero_I = 24.953
